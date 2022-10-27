e5ccbc9aa2ed96b1e4ae0bda4d86ab645505605f
Alex Quevedo <alex@monkey.pe>
2022-10-27 00:10:22 -0500

    Primer commit

---
+++ landing/assets/main.js
@@ -0,0 +1,5 @@
+(function(){window.onload=function(){window.setTimeout(fadeout,500);}
+function fadeout(){document.querySelector('.preloader').style.opacity='0';document.querySelector('.preloader').style.display='none';}
+window.onscroll=function(){var header_navbar=document.querySelector(".navbar-area");var sticky=header_navbar.offsetTop;var logo=document.querySelector('.navbar-brand img')
+if(window.pageYOffset>sticky){header_navbar.classList.add("sticky");logo.src='assets/images/logo/logo.svg';}else{header_navbar.classList.remove("sticky");logo.src='assets/images/logo/white-logo.svg';}
+var backToTo=document.querySelector(".scroll-top");if(document.body.scrollTop>50||document.documentElement.scrollTop>50){backToTo.style.display="flex";}else{backToTo.style.display="none";}};new WOW().init();let navbarToggler=document.querySelector(".mobile-menu-btn");navbarToggler.addEventListener('click',function(){navbarToggler.classList.toggle("active");});})();
