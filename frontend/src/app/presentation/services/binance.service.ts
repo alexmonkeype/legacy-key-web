cd2c15c2c884ed27926eda64c7b174a69abfa35f
Alex Q <alex@monkey.pe>
2023-12-27 15:28:24 -0500

    Implementando BinanceService con @binance-chain/bsc-connector

--- frontend/src/app/presentation/services/binance.service.ts
+++ frontend/src/app/presentation/services/binance.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { BscConnector, UserRejectedRequestError } from '@binance-chain/bsc-connector';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BinanceService {
+  private binance = new BscConnector({ supportedChainIds: [56, 97] });
   private _currentAccountAddress: string | null = null;
 
   private _accountAddress = new BehaviorSubject<string | null>(this._currentAccountAddress);
@@ -14,10 +16,15 @@ export class BinanceService {
 
   connect = async (): Promise<void> => {
     //TODO: Implementar cnx a wallet de Binanceƒ
-    return new Promise((resolve, reject) => {
+    const connector = await this.binance.activate();
+    if (connector.account) {
+      this.setAccountAddress(connector.account);
+    }
+
+    /* return new Promise((resolve, reject) => {
       this.setAccountAddress("Fake address");
       resolve();
-    });
+    }); */
   }
 
   private setAccountAddress(accountAddress: string | null) {
