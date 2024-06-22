5b2c2972325c06273510cde1c821022274607832
Alex Quevedo <alex@monkey.pe>
2024-06-22 16:53:54 -0500

    Updating CobroComponent

--- frontend/src/app/presentation/pages/cobro/cobro.component.ts
+++ frontend/src/app/presentation/pages/cobro/cobro.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { cobroHeir } from '../../../domain/model/legacy-contract.model';
 import { GetAccountUseCase } from '../../../domain/usecase/get-account.use-case';
-import Web3 from 'web3';
-
-declare let window: any;
+import { CollectLegacyUseCase } from '../../../domain/usecase/collect-legacy.use-case';
+import { Blockchain } from '../../../domain/type/blockchain.type';
 
 @Component({
   selector: 'app-cobro',
@@ -11,10 +10,15 @@ declare let window: any;
   styleUrls: ['./cobro.component.scss']
 })
 export class CobroComponent implements OnInit {
+  walletChain: Blockchain = "ethereum";
   wallterAddress: string | null = null;
   heir = new cobroHeir();
+  errorMessage: string | null = null;
 
-  constructor(private getAccountUseCase: GetAccountUseCase) {
+  constructor(
+    private getAccountUseCase: GetAccountUseCase,
+    private collectLegacyUseCase: CollectLegacyUseCase
+  ) {
 
   }
 
@@ -26,367 +30,35 @@ export class CobroComponent implements OnInit {
   }
 
   async cobro() {
-    console.log("paso por aca");
+    if (this.wallterAddress == null || this.heir.idWithdraw == null) {
+      return;
+    }
 
-    const SCLegacyKey = "0xb2735759f993555a2aeB4E7Cedba931ce25B6d0f";
-    const ABI1 = [
-      {
-        "inputs": [
-          {
-            "internalType": "address[]",
-            "name": "_hijos",
-            "type": "address[]"
-          },
-          {
-            "internalType": "address[]",
-            "name": "_validador",
-            "type": "address[]"
-          },
-          {
-            "internalType": "uint256",
-            "name": "_amount",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "_time",
-            "type": "uint256"
-          }
-        ],
-        "name": "newMember",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "payLegacy",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "_adLegacy",
-            "type": "address"
-          },
-          {
-            "internalType": "contract IERC20Metadata",
-            "name": "_usdt",
-            "type": "address"
-          }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-      },
-      {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": true,
-            "internalType": "address",
-            "name": "previousOwner",
-            "type": "address"
-          },
-          {
-            "indexed": true,
-            "internalType": "address",
-            "name": "newOwner",
-            "type": "address"
-          }
-        ],
-        "name": "OwnershipTransferred",
-        "type": "event"
-      },
-      {
-        "inputs": [],
-        "name": "renounceOwnership",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "uint256",
-            "name": "_amount",
-            "type": "uint256"
-          }
-        ],
-        "name": "setAmountPay",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "newOwner",
-            "type": "address"
-          }
-        ],
-        "name": "transferOwnership",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "uint256",
-            "name": "_count",
-            "type": "uint256"
-          }
-        ],
-        "name": "voteValidador",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "uint256",
-            "name": "_count",
-            "type": "uint256"
-          }
-        ],
-        "name": "withdrawHeir",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "adLegacy",
-        "outputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "amountPay",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "deathWitness",
-        "outputs": [
-          {
-            "internalType": "bool",
-            "name": "permit",
-            "type": "bool"
-          },
-          {
-            "internalType": "address",
-            "name": "padre",
-            "type": "address"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "decUSDT",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "heirCount",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "heirDetail",
-        "outputs": [
-          {
-            "internalType": "bool",
-            "name": "permit",
-            "type": "bool"
-          },
-          {
-            "internalType": "address",
-            "name": "padre",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "amount",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "id",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "memberID",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "padre",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "id",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "sign",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "validTime",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "saldo",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "witnessCount",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      }
-    ]
-    window.web3 = await new Web3(window.ethereum);
-    window.contract1 = await new window.web3.eth.Contract(ABI1, SCLegacyKey);
-
-    const idLegacy = this.heir.idWithdraw;
-    console.log(idLegacy);
-    await window.contract1.methods.withdrawHeir(idLegacy).send({ from: this.wallterAddress });
-
+    this.collectLegacyUseCase.execute({
+      walletChain: this.walletChain,
+      walletAddress: this.wallterAddress,
+      idLegacy: this.heir.idWithdraw
+    }).then(() => {
+      this.onSuccessSave();
+    }).catch(e => {
+      console.log(e);
+      this.errorMessage = e.message;
+    });
   }
 
+  onSuccessSave() {
+    this.showPopup(
+      "Guardado satisfactorio",
+      "¡Tu herencia se ha cobrado con éxito!",
+      "Continuar",
+      "success"
+    );
+  }
 
+  showPopup(title: string, description: string, button: string, icon?: string) {
+    const success = confirm(title + "\n" + description);
+    if (success) {
+      //this.goToNext();
+    }
+  }
 }
