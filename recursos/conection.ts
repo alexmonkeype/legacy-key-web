35f7bef223b454df47767c93a14972cbed9fd110
Alex Quevedo <alex@monkey.pe>
2024-06-23 01:51:09 -0500

    Actualizando estructura

--- recursos/conection.ts
+++
@@ -1,6 +0,0 @@
-import algosdk from "algosdk";
-
-const token = {"X-API-Key":"szoRVAhgPF1zVTIscOEYU3TmjsM5eqoD4mcwZtlp"}
-const server = 'https://testnet-algorand.api.purestake.io/ps2'
-const port = ""
-export const algodClient = new algosdk.Algodv2(token,server,port);
