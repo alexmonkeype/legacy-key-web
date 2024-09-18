054761c2d9ee076604ea8fece9497ef96780b0fd
Alex Quevedo <alex@monkey.pe>
2024-09-18 00:24:13 -0500

    update Angular 16

--- frontend/src/app/presentation/guards/auth.guard.ts
+++ frontend/src/app/presentation/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PeraWalletService } from '../../data/service/pera-wallet/pera-wallet.service';
 import { MetamaskWalletService } from '../../data/service/metamask-wallet/metamask-wallet.service';
@@ -9,7 +9,7 @@ import { AuthService } from '../../data/service/auth/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private router: Router,
     private authService: AuthService
